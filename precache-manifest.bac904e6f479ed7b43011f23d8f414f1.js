e63e49c1d398aa50e62f4900baf20c6602fec75b
NusratRahman <nusrat101280@gmail.com>
2019-12-24 00:54:07 -0600

    Updates

---
+++ precache-manifest.bac904e6f479ed7b43011f23d8f414f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e0dd414f01abf14fa0340694a7abb4",
+    "url": "/comit-final-project/index.html"
+  },
+  {
+    "revision": "e772a01894d96c707eb0",
+    "url": "/comit-final-project/static/css/main.a6b7552b.chunk.css"
+  },
+  {
+    "revision": "29b6dc691c36069a69d8",
+    "url": "/comit-final-project/static/js/2.88de1135.chunk.js"
+  },
+  {
+    "revision": "e772a01894d96c707eb0",
+    "url": "/comit-final-project/static/js/main.d72aaab9.chunk.js"
+  },
+  {
+    "revision": "09b69aa0636139b2bcfa",
+    "url": "/comit-final-project/static/js/runtime-main.9a38cf00.js"
+  }
+]);
